211914787e49674d1267f3f5e5ab35f65d1e6748
MatheusL-Dev <darkaixxt@gmail.com>
2025-04-02 00:55:51 -0300

    feat: Cria projeto e adiciona as dependencias

---
+++ styles/Themes.jsx
@@ -0,0 +1,240 @@
+'use client';
+
+import { createTheme } from "@mui/material/styles";
+
+export const realDarkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
+export const darkTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#000",
+        },
+    },
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#000",
+    hover: "#cccccc",
+    background: "#f0f0f0",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const darkGrayTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#242424",
+        },
+    },
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#242424",
+    textBodyColor: "#242424",
+    textSmallColor: "#242424",
+    fontColor: "#242424",
+    navbar: "#242424",
+    hover: "#cccccc",
+    background: "#f0f0f0",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const redTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#eb3d35",
+    hover: "#ffd6d4",
+    background: "#fff0f0",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const blueTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#1862d9",
+    hover: "#a4bfeb",
+    background: "#d9e7ff",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const greenTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#1C8A7F",
+    hover: "#93e6d2",
+    background: "#defff7",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const yellowTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#ffab00",
+    hover: "#fff59d",
+    background: "#fffce0",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#000",
+});
+
+export const purpleTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#4a148c",
+    hover: "#e1bee7",
+    background: "#fce8ff",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const pinkTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#ff1ca0",
+    hover: "#ffb5e0",
+    background: "#ffedf7",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const darkBlueTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#162447",
+    hover: "#8cb4e6",
+    background: "#e6f1ff",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const darkGreenTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#004d40",
+    hover: "#b2dfdb",
+    background: "#e0f2f1",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const greenLightTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#01cc88",
+    hover: "#b9ffe8",
+    background: "#e9fff8",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const greyTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#698996",
+    hover: "#b0bec5",
+    background: "#eceff1",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
+
+export const seaBlueTheme = createTheme({
+    typography: {
+        fontFamily: "Lato, sans-serif",
+    },
+    body: "#fff",
+    textTitleColor: "#000",
+    textBodyColor: "#000",
+    textSmallColor: "#000",
+    fontColor: "#000",
+    navbar: "#006064",
+    hover: "#b2ebf2",
+    background: "#e0f7fa",
+    sidebarBackground: "#fff",
+    backgroundPageContainer: "#fff",
+    contrastText: "#fff",
+});
